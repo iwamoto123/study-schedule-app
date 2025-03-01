90c57c3bca97c3d692391d5fba46528606faf40d
iwamoto123 <103933392+iwamoto123@users.noreply.github.com>
2025-03-01 12:16:01 +0900

    複数ページ設定

--- app/log/[id]/page.tsx
+++ app/log/[id]/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
+"use client"
+import { useParams } from "next/navigation";
+
+export default function LogDetail() {
+  const params = useParams()
+  const id = params.id;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>hello world</h1>
+        <h1>hello rog detail {id}</h1>
       </main>
     </div>
   );
